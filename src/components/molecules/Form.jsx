eef5aa1e63f5c905f42871aab696a8ae6c0da010
StefanieAviles <stefanie42019@gmail.com>
2022-07-26 14:46:39 -0500

    Validaciones version 1

--- src/components/molecules/Form.jsx
+++ src/components/molecules/Form.jsx
@@ -2,31 +2,44 @@ import { useRef } from 'react'
 import { Button } from '../atoms/Button/Button'
 import users from '../../Users.json'
 import { useNavigate, useLocation } from 'react-router-dom'
+import { useState } from 'react'
 
 export function Form() {
   const userInputRef = useRef()
   const userPasswordRef = useRef()
   let navigate = useNavigate()
   let location = useLocation()
+  const [errorUser, setErrorUser] = useState('')
+  const [errorPassword, setErrorPassword] = useState('')
   const go = () => {
+    setErrorUser('')
+    setErrorPassword('')
     const userName = userInputRef.current.value
     const password = userPasswordRef.current.value
-    let results = users.Users.filter(
-      (user) => user.username === userName && user.password === password
-    )
-    if (results.length > 0) {
-      navigate('/home' + location.search)
-    } else {
-      alert('INCORRECTO')
+    if (!userName) {
+      setErrorUser('*Campo es requerido')
+    }
+    if (!password) {
+      setErrorPassword('*Campo es requerido')
+    }
+    if (userName !== '' && password !== '') {
+      let results = users.Users.filter(
+        (user) => user.username === userName && user.password === password
+      )
+      if (results.length > 0) {
+        navigate('/home' + location.search)
+      } else {
+        alert('CREDENCIALES INCORRECTAS')
+      }
     }
   }
   return (
     <form className="form">
       <input type="text" placeholder="Usuario" ref={userInputRef}></input>
-      <span></span>
+      <span>{errorUser}</span>
       <br></br>
       <input type="password" placeholder="Contrasena" ref={userPasswordRef}></input>
-      <span></span>
+      <span>{errorPassword}</span>
       <br></br>
       <Button color="primary" size="small" onClick={go}>
         LogIn
--- src/components/molecules/FormRegister.jsx
+++ src/components/molecules/FormRegister.jsx
@@ -1,6 +1,7 @@
 import { useRef } from 'react'
 import { Button } from '../atoms/Button/Button'
 import { useNavigate, useLocation } from 'react-router-dom'
+import { useState } from 'react'
 export function FormRegister() {
   const usernameRef = useRef()
   const passwordRef = useRef()
@@ -8,7 +9,15 @@ export function FormRegister() {
   const emailRef = useRef()
   let navigate = useNavigate()
   let location = useLocation()
+  const [errorUser, setErrorUser] = useState('')
+  const [errorEmail, setErrorEmail] = useState('')
+  const [errorPassword, setErrorPassword] = useState('')
+  const [errorPasswordConf, setErrorPasswordConf] = useState('')
   const go = () => {
+    setErrorUser('')
+    setErrorPassword('')
+    setErrorEmail('')
+    setErrorPasswordConf('')
     const userName = usernameRef.current.value
     const password = passwordRef.current.value
     const passwordConfirm = passwordConfRef.current.value
@@ -18,22 +27,33 @@ export function FormRegister() {
       alert('REGISTRO CORRECTO')
       navigate('/' + location.search)
     } else {
-      alert('INCORRECTO')
+      if (!userName) {
+        setErrorUser('*Campo es requerido')
+      }
+      if (!password) {
+        setErrorPassword('*Campo es requerido')
+      }
+      if (!passwordConfirm) {
+        setErrorPasswordConf('*Campo es requerido')
+      }
+      if (!email) {
+        setErrorEmail('*Campo es requerido')
+      }
     }
   }
   return (
     <form className="form">
       <input type="text" placeholder="Usuario" ref={usernameRef}></input>
-      <span></span>
+      <span>{errorUser}</span>
       <br></br>
       <input type="text" placeholder="E-mail" ref={emailRef}></input>
-      <span></span>
+      <span>{errorEmail}</span>
       <br></br>
       <input type="password" placeholder="Contrasena" ref={passwordRef}></input>
-      <span></span>
+      <span>{errorPassword}</span>
       <br></br>
       <input type="password" placeholder="Confirmar Contrasena" ref={passwordConfRef}></input>
-      <span></span>
+      <span>{errorPasswordConf}</span>
       <br></br>
       <Button color="primary" size="small" onClick={go}>
         LogIn
